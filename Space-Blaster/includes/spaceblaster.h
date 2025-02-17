703a5eadd307e6574a4554667ddbedcd6a05a48a
AbrarShakhi <114344002+AbrarShakhi@users.noreply.github.com>
2025-02-17 17:48:33 +0600

    fix: #2 basic game demo

--- Space-Blaster/includes/spaceblaster.h
+++ Space-Blaster/includes/spaceblaster.h
@@ -1,8 +1,39 @@
 #pragma once
 
-// #ifndef SPACE_BLASTER
-// #error "Compilation ERROR 'SPACE_BLASTER' not defined
-// #elif
-#define OLC_PGE_APPLICATION
+#ifndef SPACE_BLASTER_H
+#define SPACE_BLASTER_H
+
 #include "olcPixelGameEngine.h"
-// #endif
+
+
+class SpaceBlaster : public olc::PixelGameEngine {
+   public:
+	SpaceBlaster();
+	~SpaceBlaster() = default;
+
+   private:
+	struct sSpaceObject {
+		float x = 0.0f;
+		float y = 0.0f;
+		float vx = 0.0f;
+		float vy = 0.0f;
+		float angle = 0.0f;
+		bool bDead = false;
+	};
+
+	std::list<sSpaceObject> listProjectiles;
+	std::list<sSpaceObject> listEnemies;
+	sSpaceObject player;
+	float fPlayerThrust = 100.0f;
+	float fPlayerAngle = 0.0f;
+	float fPlayerAngularSpeed = 5.0f;
+	int nScore = 0;
+
+
+   public:
+	bool OnUserCreate() override;
+
+	bool OnUserUpdate(float fElapsedTime) override;
+};
+
+#endif  // !SPACE_BLASTER_H
--- Space-Blaster/src/main.cpp
+++ Space-Blaster/src/main.cpp
@@ -1,30 +1,10 @@
+#define OLC_PGE_APPLICATION
 #include "spaceblaster.h"
-
-class SpaceBlaster : public olc::PixelGameEngine {
-   public:
-	SpaceBlaster() { sAppName = "Space Blaster"; }
-
-   public:
-	bool OnUserCreate() override {
-		// Called once at the start, so create things here
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override {
-		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y,
-				     olc::Pixel(rand() % 255, rand() % 255, rand() % 255));
-		return true;
-	}
-};
 
 
 int main() {
 	SpaceBlaster game;
 	if (game.Construct(256, 240, 4, 4))
 		game.Start();
-
 	return 0;
 }
