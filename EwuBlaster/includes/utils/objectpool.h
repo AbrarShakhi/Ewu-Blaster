e2bbe36fd1fbc85fa470b3db3aaff7ef9423c0ac
Hemonto839 <arkaroy839@gmail.com>
2025-03-16 18:53:50 +0600

    create an object pool

--- EwuBlaster/includes/utils/objectpool.h
+++ EwuBlaster/includes/utils/objectpool.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "olcPixelGameEngine.h"
+#include <vector>
+
+
+template <typename T>
+class objectPool {
+private:
+	std::vector<T*> object_pools;
+	std::vector<T*> allocated; // for tracking free space 
+
+public:
+	objectPool(size_t size);
+	~objectPool();
+
+	T* borrow();
+	void release(T* obj);
+};
